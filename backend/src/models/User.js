44e0142abdef0e37a16889dd89a31ed57286f44a
Sandeep Kumar <sandeepkumar57982@gmail.com>
2025-12-13 01:13:38 +0530

    user schema

---
+++ backend/src/models/User.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs"
+
+const userSchema =  new mongoose.Schema({
+    fullName:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6,
+    },
+    bio:{
+        type:String,
+        default:"",
+    },
+    profilePic:{
+        type:String,
+        default:""
+    },
+    nativeLanguage:{
+        type:String,
+        default:""
+    },
+    learningLanguage:{
+        type:String,
+        default:""
+    },
+    Location:{
+        type:String,
+        default:""
+    },
+    isOnboarded:{
+        type:Boolean,
+        defaut:false,
+    },
+    friends:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+        }
+    ]
+
+},  
+    {timestamps:true}   
+);
+
+const user = mongoose.model("user",userSchema);
+// to explain this one again in later video 
+// prehook so that our password is encypted
+// it just try to hash our password like 12443 =fsojvoisj that it 
+
+
+userSchema.pre("save",async function (next) {
+    if(!this.isModified("password")) return next();
+    
+    try {
+        const salt = await bcrypt.gensalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+
+        next();
+    } catch (error) {
+        next(error)
+    }
+})
